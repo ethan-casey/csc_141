597fb6307daadde6732360ac16d7c03d402b39bd
ethan-casey <ethancasey2@gmail.com>
2024-09-05 09:42:36 -0400

    9/5/24

--- 01_getting_started/01_03_infinite_skills.py
+++ 01_getting_started/01_03_infinite_skills.py
@@ -4,4 +4,7 @@ I do not have much, if any, experiance coding other than basic programming for l
 I would like to learn more skills so I can become a good game designer. It would also be cool if I learned like hacking skills.
 
 My dream is to create a game, mainly an rpg, turn based game, and an open world game that can inspire people to be heroes themselves
+
+It amazes me to see the different ways python is used. From just making basic websites, to use in the medical field, and of course
+being used to make games. I truly wanna learn and be able to do just about anything there is involving coding, like website building.
 '''
