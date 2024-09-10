5eb24f70519862da715abb6cf9cf2b8f80fca660
ethan-casey <ethancasey2@gmail.com>
2024-09-10 10:33:15 -0400

    9/10/24

---
+++ 04_working_with_lists/04_05_summing_a_million.py
@@ -0,0 +1,4 @@
+numbers = list(range(1, 1000001))
+print(f"{min(numbers)}")
+print(f"{max(numbers)}")
+print(f"{sum(numbers)}")
---
+++ 04_working_with_lists/04_07_threes.py
@@ -0,0 +1,5 @@
+threes = []
+for value in range(1, 11):
+    three = value * 3
+    threes.append(three)
+print(threes)
